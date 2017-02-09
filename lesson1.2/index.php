f04f5791f56eea2d9b5f2782afe7908fadc51561
Alex T <contact@rwsite.ru>
2017-02-09 20:57:56 +0300

    Revert "Revert "эмм""
    This reverts commit 424bb7848d5d779c60427cb4ed95c56af9e57c79.

---
+++ lesson1.2/index.php
@@ -0,0 +1,36 @@
+ <html>
+  <head>
+    <title>2 lesson</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    </head>
+
+  <body>
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12 col-sm-12 col-lg-8">
+          <div class="panel panel-primary">
+              <div class="panel-heading">
+                <h3 class="panel-title"> Введите данные</br></h3>
+                <form action="functions.php" method="post">
+                <div class="input-group">
+                          <span class="input-group-addon" id="user1">@</span>
+                          <input name="usr1" type="text" class="form-control" placeholder="Введите число" aria-describedby="basic-addon1">
+                </div>
+                <!--<div class="input-group">
+                    <span class="input-group-addon" id="user2">@</span>
+                    <input name="user2" type="text" class="form-control" placeholder="User 2" aria-describedby="basic-addon1">
+              </div>-->
+              <div class="submit">
+                <input name="submit" type="submit" class="btn center" value="Отправить" >
+              </div>
+              </form>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+  <body>
+  </html>
