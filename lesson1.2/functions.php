117ac2b55b1c52ebceeaba040a49e8f3ba08f747
Alex T <contact@rwsite.ru>
2017-02-15 20:06:55 +0300

    Все засунуто в 1 файл

--- lesson1.2/functions.php
+++ lesson1.2/functions.php
@@ -1,42 +1,91 @@
-<?php
-echo '<center style="margin-top:1%;background:#e8e8e8;padding: 20%;"><h3>';
-$u1 = $_POST["usr1"]; #число пользователя
-// Проверять не будем
-
-echo 'Ваше число: ', $u1 , '</br>';
-
-/*
-Создаем 2 переменные, задаем их значения по 1*/
-$p1=1;
-$p2=1;
-
-$flag=true;
-
-do
-{
-  if($p1>$u1)
-    { #p1 больше чем число пользователя?
-    echo 'задуманное число Не входит в числовой ряд';# Да
-    $flag = false;
-    return;
-    }
-  else
-    { #переменная меньше или равна u1
-
-      if($p1==$u1 )#переменная рвана u1
-        {
-        echo 'задуманное число входит в числовой ряд';
-        $flag = false;
-        }
-      elseif ($p1<$u1) {
-        #переменная меньше u1
-        $p3 = $p1; #сохраняем в p3 значение p1, записываем справо на лево
-        $p1 = $p2 + $p1; #добавляем к p1 значение p2, записываем в p1
-        $p2 = $p3; #присваиваем переменной p2 значение p3
-        }
-    }
+ <html>
+  <head>
+    <title>2 lesson</title>
+<style type="text/css">
+    .submit {
+    margin: 0 auto;
+    text-align: center;
+    padding-top: 20px;
+    color: black;
 }
-while($flag ); #выполняем пока "true"
+</style>
 
-echo '</h3></center>';
-?>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    </head>
+    
+    <body>
+    <div class="container">
+      <div class="row" style="padding-top: 100px;">
+        <div class="col-xs-12 col-sm-12 col-lg-8">
+          <div class="panel panel-primary">
+             
+                    <div class="panel-heading">
+                <p><h3 class="panel-title">Проверьте, входит ли ваше число в числовой ряд</h3></p>
+                <form method="post">
+                <div class="input-group">
+                          <span class="input-group-addon" id="user1">#</span>
+                          <input name="usr1" type="text" class="form-control" placeholder="Введите число" aria-describedby="basic-addon1">
+                </div>
+              <div class="submit">
+                <input name="submit" type="submit" class="btn center" value="Отправить" >
+              </div>
+              </form>
+              </div>
+            </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-12">
+           <p>
+                 <?php
+                    (int)$u1 = $_POST["usr1"]; #число пользователя
+                    echo '<strong>Ваше число:</strong> ', $u1 , '</br>';
+                    /*
+                    Создаем 2 переменные, задаем их значения по 1*/
+                    $p1=1;
+                    $p2=1;
+                    $flag=true;//Создаем флаг, для выхода из цикла
+                    /*Почему при указании типа === появляются ошибки?*/
+                    do
+                    {
+                      if($p1>$u1)
+                        { //p1 больше чем число пользователя?
+                        /*отступление 0 - входит в ряд в Фибоначчи
+                        if ($u1==0) {
+                          echo 'задуманное число 0 входит в числовой ряд';# Да
+                        } else*/
+                        echo 'задуманное число Не входит в числовой ряд';# Да
+                        $flag = false;
+                        break;
+                        }
+                      else
+                        { #переменная меньше или равна u1
+                          if($p1==$u1 )#переменная рвана u1
+                            {
+                            echo 'задуманное число входит в числовой ряд';
+                            $flag = false;
+                            }
+                          elseif ($p1<$u1) {
+                            #переменная меньше u1
+                            $p3 = $p1; #сохраняем в p3 значение p1, записываем справо на лево
+                            $p1 = $p2 + $p1; #добавляем к p1 значение p2, записываем в p1
+                            $p2 = $p3; #присваиваем переменной p2 значение p3
+                            }
+                        }
+                    }
+                    while($flag); #выполняем пока "true"
+                ?>
+            </p>           
+          </div>        
+          <div class="col-sm-12">
+          Чи́сла Фибона́ччи — элементы последовательности
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, … (последовательность A000045 в OEIS),
+в которой первые два числа равны либо 1 и 1, либо 0 и 1, а каждое последующее число равно сумме двух предыдущих чисел. Названы в честь средневекового математика Леонардо Пизанского (известного как Фибоначчи).
+<a href='https://ru.wikipedia.org/wiki/%D0%A7%D0%B8%D1%81%D0%BB%D0%B0_%D0%A4%D0%B8%D0%B1%D0%BE%D0%BD%D0%B0%D1%87%D1%87%D0%B8'>Подробнее тут</a>
+           </div>
+        </div>
+    </div>
+    </div>
+  <body>
+  </html>
---
+++ lesson1.3/index.php
@@ -0,0 +1,204 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', true);
+ini_set('html_errors', false);
+
+/*
+$europe = ['Песец','Белый медведь','Волк','Олень','Бобр'];
+$asia = ['Панда','Розовый пеликан','Павлин','Японский макак','Золотой фазан','Черная пантера'];
+$africa= ['Слон','Носорог','Жираф','Зебра','Лев','Верблюд','Буйвол','Антилопа'];
+$north_america = ['Тапир','Волк','Опоссум','Скунс','Дикобраз','Аллигатор','Гризли','Броненосец'];
+$south_america = ['Гарпия','Ягуар','Анаконда','Оцелот','Муравьед','Игуана','Тукан'];
+$ausralia = ['Кенгуру','Варан','Утконос','Намбат','Молох','Ехидна','Вомбат','Сахарный летающий поссум'];
+$antarctica = ['Пингвин','Кашалот','Морской слон','Морской леопард','Тюлень крабоед','Кальмары','Буревестник']; */
+
+
+
+/* STEP 1
+Берем строку и создаем из нее массив*/
+/*
+   Строка, которая не содержит разделителя, будет
+   просто возвращать массив с одним значением оригинальной строки.
+   В двойных кавычках интерпретатор считывает значение переменной внутри строки
+*/
+(string)$input_europe = "Vulpes lagopus,Ursus maritimus,Castor fiber,Castor";
+(string)$input_asia = "Ailuropoda melanoleuca,Pelecanus onocrotalus,Pavo cristatus,Macaca fuscata";
+(string)$input_africa= "Loxodonta africana,Diceros bicornis,Giraffa camelopardalis";
+
+
+
+/* STEP 2
+Задача: Разбить животных по континентам
+Создаем двухуровневый массив Континент ($data_continents), куда записываем все хживотных
+$data_continents = [
+    "Европпа" => $data_europe,
+    "Азия" => $data_asia,
+    "Африка" => $data_africa,
+];
+*/
+
+/* STEP 3
+Задача: Найти словосочетания разделенные пробелами
+Решение: Разобьем каждую строку на подстроки - словосочетания отделенные '',"
+Подсчитаем количество слов в строке
+Условие, если количество слов в строке >=2 , то запишем их в новую переменную
+и выведем на экран
+*/
+
+(string)$input_rezdelitel = ',';//Обозначаем разделитель
+(array)$data_europe = explode( ',', $input_europe ) ;//Создадим простые массивы из значений строк без разбивки
+(array)$data_asia = explode( ',', $input_asia );
+(array)$data_africa = explode( ',', $input_africa ) ;
+//string implode ( string $glue , array $pieces )
+
+$input_europe = split(" ",$input_europe); //считаю количество слов разделенных пробелами
+
+var_dump ($input_europe);
+echo "Количество слов в строке = " , sizeof($input_europe) ;//4
+
+
+$numb_element_europe = count($data_europe);//4
+$numb_element_asia = count($data_asia);//4
+$numb_element_africa = count($data_africa);
+
+
+
+//mb_strlen(); Подсчет символов в строке
+
+
+
+
+
+
+
+
+
+  /* STEP 4
+  Задача: Составить нове комбинации из отобранных слов и вывести их на экран
+  Решение:
+  */
+
+
+
+
+
+
+
+
+
+
+
+$europe = ['Vulpes lagopus','Ursus maritimus', 'Castor fiber'];
+$asia = ['Ailuropoda melanoleuca','Pelecanus onocrotalus','Pavo cristatus','Macaca fuscata'];
+$africa= ['Loxodonta africana','Diceros bicornis','Giraffa camelopardalis'];
+$north_america = ['Dasypus pilosus','Canis lupus','Caluromys philander'];
+$south_america = ['Morphnus guianensis','Panthera onca','Eunectes murinus','Leopardus pardalis','Myrmecophaga tridactyla'];
+$ausralia = ['Macropus','Varanus varius','Ornithorhynchus anatinus'];
+
+// Создаем массив. key может быть либо типа integer, либо типа string. value может быть любого типа.
+
+$continents = [
+    "Европпа" => $europe,
+    "Азия" => $asia,
+    "Африка" => $africa,
+    "Северная америка" => $north_america,
+    "Южная америка" => $south_america,
+    "Австралия" => $ausralia
+    ];
+    $continents;
+    //Переменная содержит двумерный массив из 6 континентов и список животных, на них обитающих
+
+   #Неработает с двумерным массивом var_dump($continents);
+// for ($i=0; $i < count($continents); $i++){
+//     echo $continents[$i];
+//     echo '</br>';
+// };
+/*foreach (массив as $ключ=>$значение)
+команды;*/
+?>
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="stylesheet" href="http://bootstrap-3.ru/examples/grid/grid.css">
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+<title>   Животные планеты Земля </title>
+</head>
+<body>
+ <div class="container">
+<div class='row'>
+  <div class="col-sm-12" style="margin: 10px 0 0 0;">
+      <?php ?>
+  </div>
+</div>
+
+    <div class="page-header">
+        <h1>Животные планеты Земля</h1>
+        <p class="lead">Эта страница посвящена животным планеты Земля.</p>
+    </div>
+
+      <h3>Все животные в базе</h3>
+      <p>Выведем <strong>всех животных</strong> из базы на экран</p>
+    <div class="row">
+        <div class="col-md-4">
+          <h4>Европа</h4>
+          <?php  for ($i=0; $i < count($europe); $i++){
+        echo $europe[$i];    echo '</br>';     }; ?>
+      </div>
+        <div class="col-md-4">
+        <h4>Азия</h4>
+        <?php  for ($i=0; $i < count($asia); $i++){
+        echo $asia[$i];    echo '</br>';     }; ?>
+      </div>
+        <div class="col-md-4">
+        <h4>Африка</h4>
+        <?php for ($i=0; $i < count($africa); $i++){
+        echo $africa[$i];    echo '</br>';     }; ?>
+      </div>
+    </div>
+       <hr>
+    <div class="row">
+       <div class="col-md-4">
+         <h4>Северная Америка</h4>
+         <?php   for ($i=0; $i < count($north_america); $i++){
+        echo $north_america[$i];   echo '</br>';   }; ?>
+      </div>
+       <div class="col-md-4">
+         <h4>Южная Америка</h4>
+         <?php   for ($i=0; $i < count($south_america); $i++){
+        echo $south_america[$i];    echo '</br>';     }; ?>
+      </div>
+        <div class="col-md-4">
+          <h4>Австралия</h4>
+        <?php   for ($i=0; $i < count($ausralia); $i++){
+        echo $ausralia[$i];    echo '</br>';     }; ?>
+      </div>
+    </div>
+      <hr>
+      <h3>Offset, push, and pull resets</h3>
+      <p>Reset offsets, pushes, and pulls at specific breakpoints.</p>
+      <div class="row">
+        <div class="col-sm-5 col-md-6">.col-sm-5 .col-md-6</div>
+        <div class="col-sm-5 col-sm-offset-2 col-md-6 col-md-offset-0">.col-sm-5 .col-sm-offset-2 .col-md-6 .col-md-offset-0</div>
+      </div>
+      <div class="row">
+        <div class="col-sm-6 col-md-5 col-lg-6">.col-sm-6 .col-md-5 .col-lg-6</div>
+        <div class="col-sm-6 col-md-5 col-md-offset-2 col-lg-6 col-lg-offset-0">.col-sm-6 .col-md-5 .col-md-offset-2 .col-lg-6 .col-lg-offset-0</div>
+      </div>
+    </div> <!-- /container -->
+
+  </body>
+
+</body>
+</html>
